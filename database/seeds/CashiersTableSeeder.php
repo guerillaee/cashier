57394e1dfe072b29bfdbd664df2209eaddcd8c9b
Evheniy Bordiuk <ev.bordiuk@gmail.com>
2018-06-21 07:03:19 +0300

    Added Cashier model, migration, seeder

---
+++ database/seeds/CashiersTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CashiersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('cashiers')->truncate();
+       DB::table('cashiers')->insert(['name' => 'Cashier']);
+    }
+}
